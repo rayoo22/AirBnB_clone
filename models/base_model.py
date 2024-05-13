34090ebe17b281f9c6a97b6dfe3e78d5bc0993d9
ryan kibet <ryankibet14@gmail.com>
2024-05-13 18:26:45 +0300

    recreate instance with dict. representation of itself

--- models/base_model.py
+++ models/base_model.py
@@ -6,16 +6,26 @@ from datetime import datetime
 
 class BaseModel:
     """contents of the BaseModel class"""
-    def __init__(self):
+    def __init__(self, **kwargs):
         """
         Args:
             id: id for every base_model instance createad
             created_at: attribute for datetime an instance was created
             updated_at: "" for datetime an instance was created and updated
         """
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            """if kwargs is not empty"""
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+            if 'created_at' in kwargs:
+                self.created_at = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            if 'updated_at' in kwargs:
+                self.updated_at = datetime.strptime(kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """string representation of basemodel object"""
